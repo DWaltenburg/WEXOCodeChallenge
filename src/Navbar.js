43d8795461824defef5e8a7cf065123f60826b2e
David Waltenburg <david@waltenburg.dk>
2024-05-31 20:39:18 +0200

    Basic navbar with offcanvas for small screens

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,26 +3,40 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas  from "react-bootstrap/Offcanvas";
 
 const MyNavbar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-        <Container>
-            <Navbar.Brand href="\">Wexo Film</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Link to="/" className="nav-link">Home</Link>
+        <>
+            <Navbar expand='sm' className="bg-body-tertiary mb-3">
+              <Container fluid>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+                <Navbar.Brand href="#">Wexo Film</Navbar.Brand>
+                <Navbar.Offcanvas
+                  id={`offcanvasNavbar-expand-sm`}
+                  aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+                  placement="start"
+                >
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
+                      Wexo Film
+                    </Offcanvas.Title>
+                  </Offcanvas.Header>
+                  <Offcanvas.Body>
+                    <Nav className="flex-grow-1 pe-3">
+                    <Link to="/" className="nav-link">Home</Link>
                 <NavDropdown title="Genres" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/genre">Genres</NavDropdown.Item>
+                <NavDropdown.Item href="/genre">Genres (Not Implemented Yet)</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="/genre#variableidorname">Specific Genre</NavDropdown.Item>
                 <NavDropdown.Item href="/genre#anotherone">Should be Dynamicly generated</NavDropdown.Item>
                 </NavDropdown>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
-        </Navbar>
-    );
+                    </Nav>
+                  </Offcanvas.Body>
+                </Navbar.Offcanvas>
+              </Container>
+            </Navbar>
+        </>
+      );
 }
 export default MyNavbar;
